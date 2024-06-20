e2c6cc1688ecdf29e26857f5a73d2d690f15fdc1
Warren Acosta <infinitycode001@gmail.com>
2024-06-20 14:03:30 -0400

    adding dotenv dependency

--- models/mysql/connection.js
+++ models/mysql/connection.js
@@ -1,11 +1,14 @@
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv/config';
 
+// MySQL connection
 const config = {
-    host: process.env.DB_HOST || 'localhost',
-    port: process.env.DB_PORT || 3306,
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'root',
-    database: process.env.DATABASE || 'makeitdb',
-};
+    host: process.env.MYSQL_ADDON_HOST || 'localhost',
+    port: process.env.MYSQL_ADDON_POR || 3306,
+    user: process.env.MYSQL_ADDON_USER || 'root',
+    password: process.env.MYSQL_ADDON_PASSWORD || 'root',
+    database: process.env.MYSQL_ADDON_DB || 'makeitdb',
+}
+
 
 export const connection = await mysql.createConnection(config);
