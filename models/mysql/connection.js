a98837d9f053f5b29518a2131c6d5bdb51de39b8
Warren Acosta <infinitycode001@gmail.com>
2024-06-19 23:08:58 -0400

    adding envirotmen variables

--- models/mysql/connection.js
+++ models/mysql/connection.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const config = {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'root',
-    database: 'makeitdb'
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 3306,
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || 'root',
+    database: process.env.DATABASE || 'makeitdb'
 };
 
 export const connection = await mysql.createConnection(config);
