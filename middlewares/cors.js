ca67e083fd2e2c269e8c71a970a8d77e96365966
Warren Acosta <infinitycode001@gmail.com>
2024-06-20 20:12:21 -0400

    cors change

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,6 @@ import cors from 'cors'
 
 const ACCEPTED_ORIGINS = [
     'http://localhost:5173',
-    'https://x59qt5dp-5173.use2.devtunnels.ms/'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) => cors({
