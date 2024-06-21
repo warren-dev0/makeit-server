d633dbb642b1ba21abb1ac0dc00b86e9aad6bc8c
Warren Acosta <infinitycode001@gmail.com>
2024-06-20 20:21:14 -0400

    trying different cors

--- app.js
+++ app.js
@@ -6,16 +6,23 @@ import { createUsersRouter } from './routes/users.js';
 import { createQuestionsRouter } from './routes/questions.js';
 import { createTasksStatusRouter } from './routes/tasks-status.js'
 import { createTasksGroupRouter } from './routes/tasks-group.js';
-import { corsMiddleware } from './middlewares/cors.js';
+// import { corsMiddleware } from './middlewares/cors.js';
 
 export const createApp = ({ taskModel, subTaskModel, userModel, questionModel, taskStatusModel, taskGroupModel }) => {
     const app = express();
     app.disable('x-powered-by');
     app.use(express.json());
-    app.use(corsMiddleware());
+    // app.use(corsMiddleware());
+    app.use((req, res, next) => {
+        res.set('Access-Control-Allow-Credentials', 'true');
+        res.set('Access-Control-Allow-Origin', 'http://localhost:5173');
+        res.set('Access-Control-Allow-Headers', 'Content-Type');
+        res.set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+        next();
+    })
 
     // Api routes
-    
+
     app.use('/api/tasks', createTasksRouter({ taskModel }));
     app.use('/api/sub-tasks', createSubTasksRouter({ subTaskModel }));
     app.use('/api/users', createUsersRouter({ userModel }));
@@ -26,9 +33,9 @@ export const createApp = ({ taskModel, subTaskModel, userModel, questionModel, t
     app.use(function (req, res, next) {
         res.status(404).send("<h1>Sorry nothing found!<h1>");
     });
-    
+
     const PORT = process.env.PORT ?? 1234;
-    
+
     app.listen(PORT, () => {
         console.log(`server is running on port http://localhost:${PORT}`);
     });
