df79a09f85bc8ea329ecf6926782f1047e4cf93c
Warren Acosta <infinitycode001@gmail.com>
2024-09-09 21:21:05 -0400

    fixing cors 1

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,3 +1,9 @@
 import cors from 'cors';
 
-export const corsMiddleware = () => cors({origin: '*'})
+export const corsMiddleware = () => {
+    return cors({
+        origin: '*',
+        methods: 'GET,POST,PUT,DELETE',
+        allowedHeaders: 'Content-Type,Authorization',
+    });
+}
