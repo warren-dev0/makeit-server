774c1b5c8e1adeb282db1917761d473210b6320c
Warren Acosta <infinitycode001@gmail.com>
2024-09-09 21:00:06 -0400

    allowing all origins

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,14 +1,3 @@
 import cors from 'cors';
 
-export const corsMiddleware = ({ acceptedOrigins } = {}) => cors({
-    origin: (origin, callback) => {
-        
-        if (acceptedOrigins.includes(origin)) return callback(null, true);
-
-        if (!origin) return callback(null, true);
-
-        return callback(new Error('The origin is not allowed'));
-    }
-})
-
-// export const corsMiddleware = () => cors({origin: '*'})
+export const corsMiddleware = () => cors({origin: '*'})
